0e511ef30ac71e8b6e45ecbd62328ab639e70841
Rustam <sypooit@gmail.com>
2025-06-02 00:42:00 +0300

    format

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,5 +3,5 @@ from utils import camel_case_to_snake_case
 
 def test_camel_case_to_snake_case():
     assert camel_case_to_snake_case("SomeSDK") == "some_sdk"
-    assert camel_case_to_snake_case("RServoDrive") == 'r_servo_drive'
-    assert camel_case_to_snake_case("SDKDemo") == 'sdk_demo'
+    assert camel_case_to_snake_case("RServoDrive") == "r_servo_drive"
+    assert camel_case_to_snake_case("SDKDemo") == "sdk_demo"
